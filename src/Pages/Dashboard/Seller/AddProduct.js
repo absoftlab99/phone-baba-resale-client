08f47de103029b1ce3da1419ce65d9c6eac1b47c
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2022-11-27 13:30:53 +0600

    registration form customized

--- src/Pages/Dashboard/Seller/AddProduct.js
+++ src/Pages/Dashboard/Seller/AddProduct.js
@@ -4,8 +4,8 @@ const AddProduct = () => {
     return (
         <div>
             <h1 className='text-3xl py-4'>Product Add Form</h1>
-            <form className='w-8/12 mx-auto'>
-                <div className="form-control grid grid-cols-2 gap-16">
+            <form className='md:w-8/12 sm:w-full mx-auto'>
+                <div className="form-control grid sm:grid-cols-1 md:grid-cols-2 gap-4 md:gap-16">
                     <div className=''>
                     <label className="label">
                         <span className="label-text">Product Name</span>
@@ -14,13 +14,13 @@ const AddProduct = () => {
                     </div>
                     <div>
                     <label className="label">
-                        <span className="label-text">Price</span>
+                        <span className="label-text">Sale Price</span>
                     </label>
-                    <input type="text" placeholder="price" className="input input-bordered w-full" />
+                    <input type="number" placeholder="sale prise" className="input input-bordered w-full" />
                     </div>
                 </div>
 
-                <div className="form-control grid grid-cols-2 gap-16">
+                <div className="form-control grid grid-cols-2 gap-4 md:gap-16">
                     <div className=''>
                     <label className="label">
                         <span className="label-text">Contact</span>
@@ -35,29 +35,47 @@ const AddProduct = () => {
                     </div>
                 </div>
 
-                <div className="form-control grid grid-cols-2 gap-16">
+                <div className="form-control grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-16">
                     <div className=''>
                     <label className="label">
                         <span className="label-text">Phone Condition</span>
                     </label>
-                    <select className="select w-full max-w-xs select-bordered">
+                    <select className="select w-full select-bordered">
                         <option disabled selected>Pick phone condition</option>
                         <option className=''>Excellent</option>
                         <option className=''>Good</option>
                         <option className=''>Fair</option>
                     </select>
                     </div>
+
+                    <div>
+                        <label className="label">
+                            <span className="label-text">Category</span>
+                        </label>
+                        <select className="select w-full select-bordered">
+                            <option disabled selected>Category</option>
+                            <option className=''>Apple</option>
+                            <option className=''>Xiaomi</option>
+                            <option className=''>Nokia</option>
+                            <option className=''>Black Berry</option>
+                            <option className=''>Samsung</option>
+                            <option className=''>Walton</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className="form-control grid grid-cols-2 gap-4 md:gap-16">
+                    <div className=''>
+                    <label className="label">
+                        <span className="label-text">Original Prise</span>
+                    </label>
+                    <input type="number" placeholder="original prise" className="input input-bordered w-full" />
+                    </div>
                     <div>
                     <label className="label">
-                        <span className="label-text">Category</span>
+                        <span className="label-text">Location</span>
                     </label>
-                    <select className="select w-full max-w-xs select-bordered">
-                        <option disabled selected>Category</option>
-                        <option className=''>iPhone</option>
-                        <option className=''>Redmi</option>
-                        <option className=''>Samsung</option>
-                        <option className=''>Techno</option>
-                    </select>
+                    <input type="text" placeholder="ex- Dhaka" className="input input-bordered w-full" />
                     </div>
                 </div>
             </form>
--- src/Pages/Home/Ads.js
+++ src/Pages/Home/Ads.js
@@ -1,19 +1,64 @@
 import React from 'react';
+import photo from '../../Assets/images/phone1.jpg';
+import { BsBookmarkCheck, BsFillCheckCircleFill } from 'react-icons/bs';
+import { BiCategory } from "react-icons/bi";
 
 const Ads = () => {
     return (
-        <div className='w-[80%] lg:w-1/2 mx-auto mt-10'>
-            <div className="h-[150px] card card-side bg-base-100 shadow-xl border">
-                <figure><img src="https://placeimg.com/200/280/arch" alt="Movie"/></figure>
-                <div className="card-body p-4">
-                    <h2 className="card-title">Product Name</h2>
-                    <p className='text-left'>Price: 200 BDT</p>
-                    <div className="card-actions justify-end">
-                    <button className="btn btn-sm btn-primary">Watch</button>
+        <section className='my-10'>
+            <h1 className='text-3xl'>Advertise Items</h1>
+            <div className='grid grid-cols-1 md:grid-cols-3 md:w-10/12 m-auto'>
+
+                <div className="card bg-base-100 shadow-xl m-5">
+                    <figure className=''><img className='' src={photo} alt="Album"/></figure>
+                    <div className="card-body">
+                        <h2 className="card-title text-left">iPhone 14 pro Max</h2>
+                        <p className='m-0 text-left'>Location: <b>Dhaka</b></p>
+                        <p className='m-0 text-left'>Resale Price: <b className='text-accent'>10,000 BDT</b></p>
+                        <p className='m-0 text-left'>Original Price: <b className='text-gray-500'>2,00,000 BDT</b></p>
+                        <p className='m-0 text-left'>Uses Duration: <b className='text-gray-500'>2 Years</b></p>
+                        <p className='m-0 text-left flex my-auto'>Owner: <b className='pl-1'>Abdullah Al Mahmud</b><span className='text-blue-600 pl-2'><BsFillCheckCircleFill></BsFillCheckCircleFill></span></p>
+                        <p className='m-0 text-left'>Publish Date: <b>10 Dec 2022</b></p>
+                        <div className="card-actions">
+                        <button className="btn btn-sm btn-primary"><BsBookmarkCheck className='mr-2'></BsBookmarkCheck>Book Now</button>
+                        <span className='btn btn-sm btn-accent'><BiCategory className='mr-2'></BiCategory> Apple</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="card bg-base-100 shadow-xl m-5">
+                    <figure className=''><img className='' src={photo} alt="Album"/></figure>
+                    <div className="card-body">
+                        <h2 className="card-title text-left">iPhone 14 pro Max</h2>
+                        <p className='m-0 text-left'>Location: <b>Dhaka</b></p>
+                        <p className='m-0 text-left'>Resale Price: <b className='text-accent'>10,000 BDT</b></p>
+                        <p className='m-0 text-left'>Original Price: <b className='text-gray-500'>2,00,000 BDT</b></p>
+                        <p className='m-0 text-left'>Uses Duration: <b className='text-gray-500'>2 Years</b></p>
+                        <p className='m-0 text-left flex my-auto'>Owner: <b className='pl-1'>Abdullah Al Mahmud</b><span className='text-blue-600 pl-2'><BsFillCheckCircleFill></BsFillCheckCircleFill></span></p>
+                        <p className='m-0 text-left'>Publish Date: <b>10 Dec 2022</b></p>
+                        <div className="card-actions">
+                        <button className="btn btn-sm btn-primary"><BsBookmarkCheck className='mr-2'></BsBookmarkCheck>Book Now</button>
+                        <span className='btn btn-sm btn-accent'><BiCategory className='mr-2'></BiCategory> Apple</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="card bg-base-100 shadow-xl m-5">
+                    <figure className=''><img className='' src={photo} alt="Album"/></figure>
+                    <div className="card-body">
+                        <h2 className="card-title text-left">iPhone 14 pro Max</h2>
+                        <p className='m-0 text-left'>Location: <b>Dhaka</b></p>
+                        <p className='m-0 text-left'>Resale Price: <b className='text-accent'>10,000 BDT</b></p>
+                        <p className='m-0 text-left'>Original Price: <b className='text-gray-500'>2,00,000 BDT</b></p>
+                        <p className='m-0 text-left'>Uses Duration: <b className='text-gray-500'>2 Years</b></p>
+                        <p className='m-0 text-left flex my-auto'>Owner: <b className='pl-1'>Abdullah Al Mahmud</b><span className='text-blue-600 pl-2'><BsFillCheckCircleFill></BsFillCheckCircleFill></span></p>
+                        <p className='m-0 text-left'>Publish Date: <b>10 Dec 2022</b></p>
+                        <div className="card-actions">
+                        <button className="btn btn-sm btn-primary"><BsBookmarkCheck className='mr-2'></BsBookmarkCheck>Book Now</button>
+                        <span className='btn btn-sm btn-accent'><BiCategory className='mr-2'></BiCategory> Apple</span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
