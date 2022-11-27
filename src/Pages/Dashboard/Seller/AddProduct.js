c2983ef20f44ae69c14d383d35285dc223076edd
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2022-11-27 09:55:10 +0600

    Category updated

--- src/Pages/Dashboard/Seller/AddProduct.js
+++ src/Pages/Dashboard/Seller/AddProduct.js
@@ -3,7 +3,64 @@ import React from 'react';
 const AddProduct = () => {
     return (
         <div>
-            <h1>This is add product page</h1>
+            <h1 className='text-3xl py-4'>Product Add Form</h1>
+            <form className='w-8/12 mx-auto'>
+                <div className="form-control grid grid-cols-2 gap-16">
+                    <div className=''>
+                    <label className="label">
+                        <span className="label-text">Product Name</span>
+                    </label>
+                    <input type="text" placeholder="product name" className="input input-bordered w-full" />
+                    </div>
+                    <div>
+                    <label className="label">
+                        <span className="label-text">Price</span>
+                    </label>
+                    <input type="text" placeholder="price" className="input input-bordered w-full" />
+                    </div>
+                </div>
+
+                <div className="form-control grid grid-cols-2 gap-16">
+                    <div className=''>
+                    <label className="label">
+                        <span className="label-text">Contact</span>
+                    </label>
+                    <input type="text" placeholder="phone number" className="input input-bordered w-full" />
+                    </div>
+                    <div>
+                    <label className="label">
+                        <span className="label-text">Location</span>
+                    </label>
+                    <input type="text" placeholder="ex- Dhaka" className="input input-bordered w-full" />
+                    </div>
+                </div>
+
+                <div className="form-control grid grid-cols-2 gap-16">
+                    <div className=''>
+                    <label className="label">
+                        <span className="label-text">Phone Condition</span>
+                    </label>
+                    <select className="select w-full max-w-xs select-bordered">
+                        <option disabled selected>Pick phone condition</option>
+                        <option className=''>Excellent</option>
+                        <option className=''>Good</option>
+                        <option className=''>Fair</option>
+                    </select>
+                    </div>
+                    <div>
+                    <label className="label">
+                        <span className="label-text">Category</span>
+                    </label>
+                    <select className="select w-full max-w-xs select-bordered">
+                        <option disabled selected>Category</option>
+                        <option className=''>iPhone</option>
+                        <option className=''>Redmi</option>
+                        <option className=''>Samsung</option>
+                        <option className=''>Techno</option>
+                    </select>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 };
